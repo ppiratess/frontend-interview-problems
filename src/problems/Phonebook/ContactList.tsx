1ff3c799bb5e1f8218f96e3bbfc21d3793a38952
ppiratess <tamrakarshubham23@gmail.com>
2025-08-23 23:03:38 +0545

    feat: show contacts count

--- src/problems/Phonebook/ContactList.tsx
+++ src/problems/Phonebook/ContactList.tsx
@@ -2,7 +2,7 @@ import type { ContactListType } from './interface';
 
 const ContactList = ({ contactList }: { contactList: ContactListType[] }) => {
   return (
-    <div className='mx-8'>
+    <div className='py-8 pl-12'>
       <span className='text-center text-xl font-bold'>Contacts</span>
 
       <div className='mt-4 grid gap-2'>
@@ -14,6 +14,11 @@ const ContactList = ({ contactList }: { contactList: ContactListType[] }) => {
           <span className='text-text-secondary text-xs'>No contacts found</span>
         )}
       </div>
+      {contactList.length > 0 && (
+        <span className='text-text-secondary pl-4 text-xs'>
+          {contactList.length} contacts
+        </span>
+      )}
     </div>
   );
 };
