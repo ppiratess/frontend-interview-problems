415f86d240117dbc0bb646a19a2a05c1e64f0b31
ppiratess <tamrakarshubham23@gmail.com>
2025-08-23 17:41:54 +0545

    feat: setup tailwind colors

--- src/layout/AppLayout.tsx
+++ src/layout/AppLayout.tsx
@@ -1,12 +1,12 @@
-import { Outlet } from "react-router";
+import { Outlet } from 'react-router';
 
-import Sidebar from "./Sidebar";
+import Sidebar from './Sidebar';
 
 const AppLayout = () => {
   return (
     <>
       <Sidebar />
-      <main className="ml-72 p-8">
+      <main className='ml-64 p-8'>
         <Outlet />
       </main>
     </>
--- src/layout/Sidebar.tsx
+++ src/layout/Sidebar.tsx
@@ -1,5 +1,11 @@
+import { NavLink } from 'react-router';
+
 const Sidebar = () => {
-  return <div className="w-72 bg-gray-300 h-screen fixed">Sidebar</div>;
+  return (
+    <div className='bg-dark-blue fixed h-screen w-64 px-4 py-12 text-white'>
+      <NavLink to={'/phone-book'}> Phonebook</NavLink>
+    </div>
+  );
 };
 
 export default Sidebar;
